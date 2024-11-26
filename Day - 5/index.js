150622dfbca978b656cd4f749d4e021c65ebb84e
Anushka Kumari <anushkakumari1962003@gmail.com>
2024-11-26 18:51:23 +0530

    version 1

--- Day - 5/index.js
+++ Day - 5/index.js
@@ -3,3 +3,18 @@ for(let i=0; i < str.length; i++){
     console.log(str[i])
 }
 
+
+check for isPalindrome(string){
+    let res = ""
+    for(let i=string.lenght; i>=0; i--){
+        res = res + string[i]
+    }
+    if(res === string){
+        return "palindrome"
+    }else{
+        return "not palindrome"
+    }
+    
+}
+let out = isPalindrome("masai");
+console.log(out)
